fbd478d030947a1599c80cd2f8c6c709f55cfe98
nancykaur11 <nancykaur1119@email.com>
2024-12-10 13:50:36 +0530

    first commit

---
+++ next.config.js
@@ -0,0 +1,19 @@
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {}
+
+// module.exports = nextConfig
+
+
+
+
+
+
+/// next.config.js
+module.exports = {
+    // This tells Next.js to generate a static HTML export of your project
+    output: 'export',
+  
+    // Other configurations like enabling React strict mode (optional but recommended)
+    reactStrictMode: true,
+  };
+  
